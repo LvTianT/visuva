ff814f6f0febe1d3385a5f3e8c8157da76be2b8b
吕恬田 <14196597+rorojump@user.noreply.gitee.com>
2025-10-01 00:30:25 +0800

    first commit

---
+++ src/main/java/generator/mapper/SpaceUserMapper.java
@@ -0,0 +1,18 @@
+package generator.mapper;
+
+import generator.domain.SpaceUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 22183
+* @description 针对表【space_user(空间用户关联)】的数据库操作Mapper
+* @createDate 2025-05-23 15:23:55
+* @Entity generator.domain.SpaceUser
+*/
+public interface SpaceUserMapper extends BaseMapper<SpaceUser> {
+
+}
+
+
+
+
---
+++ src/main/java/generator/service/SpaceUserService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import generator.domain.SpaceUser;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 22183
+* @description 针对表【space_user(空间用户关联)】的数据库操作Service
+* @createDate 2025-05-23 15:23:55
+*/
+public interface SpaceUserService extends IService<SpaceUser> {
+
+}
